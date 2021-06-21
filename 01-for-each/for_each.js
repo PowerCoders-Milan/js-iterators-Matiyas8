7db3ffbff54c1948fc62a01e726c57eaf09863d2
Matiyas8 <matnig7@gmail.com>
2021-06-21 18:03:10 +0200

    exersice 1 solved

--- 01-for-each/for_each.js
+++ 01-for-each/for_each.js
@@ -1,5 +1,6 @@
 // define an array of fruits
-
+const fruits = ['Apple', 'Orange', 'Grape', 'Mandarin', 'Banana', 'Strawberry'];
 // .forEach((fruit, index) => {
-    // console.log() a message interpolating the name of the fruit and it's position in the array (index increased by 1);
+// console.log() a message interpolating the name of the fruit and it's position in the array (index increased by 1);
 // })
+fruits.forEach((element, index) => console.log(index + 1, element));
