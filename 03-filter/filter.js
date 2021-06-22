49dbf7ce9ccc4f7f1a812c91631d8f5e0be789da
Matiyas8 <matnig7@gmail.com>
2021-06-22 10:36:19 +0200

    exersice 3 solved

--- 03-filter/filter.js
+++ 03-filter/filter.js
@@ -1,7 +1,21 @@
 // create a randomNumbers array, containing 100 random numbers
-
+const randomNumbers = [];
+for (let count = 0; count < 100; count++) {
+    randomNumbers[count] = Math.ceil(Math.random() * 100);
+}
+console.log(randomNumbers);
 // create a function isPrime(number)
+function isPrime(number) {
+    for (var i = 2; i < number; i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return number > 1;
+}
 
 // create a primeNumbers array
-const primeNumbers = // filter randomNumbers with isPrime(number)
-console.log(primeNumbers)
+const primeNumbers = randomNumbers.filter(isPrime);
+// filter randomNumbers with isPrime(number)
+console.log('Among this random Numbers the following are prime Numbers >>>>')
+console.log(primeNumbers);
